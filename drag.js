39962595960c71475824129e5af577d7fe081beb
Suyou2020 <youyoufenglai@126.com>
2017-01-01 16:55:31 +0800

    finish drag

--- drag.js
+++ drag.js
@@ -3,12 +3,17 @@ function drag(id){
 	var disX = 0;
 	var disY = 0;
 	obj.onmousedown = function(ev){
+		// alert(5);
 		var disX = ev.pageX - obj.offsetLeft;
 		var disY = ev.pageY - obj.offsetTop;
-
-		obj.onmousemove = function(ev){
+		document.onmousemove = function(ev){
 			obj.style.left = ev.pageX - disX + 'px';
 			obj.style.top = ev.pageY - disY + 'px';
 		}
+		document.onmouseup = function(){
+			document.onmousemove = null;
+			document.onmouseup = null;
+		}
+		return false;
 	}
 }
